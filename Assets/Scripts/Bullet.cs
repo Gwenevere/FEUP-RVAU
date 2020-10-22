f1c50a6badf06d337f2487e839a0086c608c203c
migudmigu <migud.carvalho@gmailcom>
2020-10-22 20:55:42 +0100

    Added Turret, Bullet, Tower

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+	public float speed;
+	public int damage;
+    private float timer;
+    private Vector3 dir = new Vector3(1,0,0);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        transform.Translate(dir.normalized * speed * Time.deltaTime);
+
+        if(timer > 3)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        Debug.Log("Bullet collided");
+
+        Destroy(gameObject);
+    }
+
+    /*
+	public void Seek (Transform _target)
+	{
+		target = _target;
+	}*/
+}
---
+++ Assets/Scripts/Turret.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turret : MonoBehaviour
+{
+    public GameObject target;
+    public int health = 200;
+    public float fireRate = 4f;
+    public float level = 1;
+    public GameObject bullet;
+    private float shoottimer = 0;
+    public Transform firePoint;
+    private Vector3 dir;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("started turret");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Shoot();
+        CheckHealth();
+        RotateToTarget();
+    }
+
+    void CheckHealth()
+    {
+        if(health<=0)
+        {
+            //Die
+            GameObject.Destroy(gameObject);
+        }
+    }
+
+    void TakeDamage(int damage)
+    {
+        health -= damage;
+    }
+
+
+    void Shoot()
+    {
+        shoottimer += Time.deltaTime;
+        if(shoottimer > 1/fireRate){
+            Debug.Log("Shoot");
+            Instantiate(bullet, firePoint.position, firePoint.rotation);
+            shoottimer = 0;
+        }
+    }
+
+    void RotateToTarget()
+    {
+        dir = target.transform.position - transform.position;
+        Quaternion lookDir = Quaternion.LookRotation(dir);
+        Vector3 rotation = Quaternion.Lerp(transform.rotation, lookDir, Time.deltaTime).eulerAngles;
+        transform.rotation = Quaternion.Euler(0, rotation.y, 0);
+        
+    }
+    
+}
