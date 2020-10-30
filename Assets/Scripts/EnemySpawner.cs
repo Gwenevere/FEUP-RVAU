7389a2965aff8e48a7169a57a943fdc1b4710635
migudmigu <migud.carvalho@gmailcom>
2020-10-30 01:44:47 +0000

    Created EnemySpawner

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -2,32 +2,56 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class EnemySpawner : MonoBehaviour
 {
     public ZombieController zombie;
     public float spawn_radius;
+    GameObject[] zombies;
+    int num_zombies;
+    int num_wave_zombies;
+    int num_courotine = 1;
+    private Coroutine currentCoroutine;
 
     // Start is called before the first frame update
     void Start()
     {
-        StartSpawning();    // Depois é o game controller que chama
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        num_wave_zombies = GameController.Instance.current_wave * 5;
+        //Debug.Log(GameController.Instance.current_wave);
+        if(num_zombies > num_wave_zombies)
+        {
+            StopCoroutine(currentCoroutine);
+        }
     }
 
-    public void StartSpawning()
+    public void StartWave()
     {
-        StartCoroutine(SpawnZombie(2f));
+        float time_between = Random.Range(2,6);
+        Debug.Log("Started wave with " + num_wave_zombies);
+        currentCoroutine = StartCoroutine(SpawnZombies(time_between));
     }
 
-    IEnumerator SpawnZombie(float time)
+    public void StopWave()
+    {
+        StopCoroutine(currentCoroutine);
+        foreach (GameObject zombie in GameObject.FindGameObjectsWithTag("Zombie"))
+        {
+            num_zombies--;
+            GameObject.Destroy(zombie);
+        }
+        GameController.Instance.NextWave();
+    }
+
+    IEnumerator SpawnZombies(float time)
     {
         while(true)
         {
+            num_zombies++;
             Debug.Log("Zombie Spawned");
             Instantiate(zombie, GenerateSpawnCoordinates(), Quaternion.identity);
             yield return new WaitForSeconds(time);
