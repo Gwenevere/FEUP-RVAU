cac5d5b5ee42b97d1044676a976b484132ff99ea
migudmigu <migud.carvalho@gmail.com>
2020-12-14 19:09:09 +0000

    Created posters database

---
+++ proj2/make_transforms.py
@@ -0,0 +1,54 @@
+import cv2
+import argparse
+import numpy as np
+import os
+
+parser = argparse.ArgumentParser(description='Read an image and make transforms and compute descriptors.')
+parser.add_argument('image', type=str,
+                    help='name of the image inside posters folder (with extension)')
+args = parser.parse_args()
+
+image_name = os.path.splitext(args.image)[0]
+extension = os.path.splitext(args.image)[1]
+img = cv2.imread("posters/" + args.image)
+
+print(img)
+
+cv2.imshow("image", img)
+cv2.waitKey(0)
+
+sift = cv2.xfeatures2d.SIFT_create()
+kp, des = sift.detectAndCompute(img, None)
+
+#orb = cv2.ORB_create(nfeatures=500)
+#kp, des = orb.detectAndCompute(img, None)
+
+img2 = cv2.drawKeypoints(img, kp, None, color=(0,255,0), flags=0)
+
+cv2.imshow("keypoints", img2)
+cv2.waitKey(0)
+
+
+# Transformations
+vertices = np.float32([
+    [0, 0],
+    [0, img.shape[0]],
+    [img.shape[1], img.shape[0]],
+    [img.shape[1], 0],
+]).reshape(-1,1,2)
+
+pts1 = np.float32([[img.shape[0]/2, img.shape[1]/2], [img.shape[0], img.shape[1]/4 + img.shape[1]/2], [img.shape[0]/2, img.shape[1]], [0, img.shape[1]/4 + img.shape[1]/2]])
+
+M = cv2.getPerspectiveTransform(vertices, pts1)
+
+dst = cv2.warpPerspective(img, M, (img.shape[0], img.shape[1]))
+
+cv2.imshow("imgtransformed", dst)
+cv2.waitKey(0)
+
+if not os.path.exists('computed_posters/' + os.path.splitext(args.image)[0]):
+    os.mkdir("computed_posters/" + os.path.splitext(args.image)[0])
+
+np.save("computed_posters/" + image_name + "/descriptor", des)
+cv2.imwrite("computed_posters/" + image_name + '/' + args.image, img)
+cv2.imwrite("computed_posters/" + image_name + '/' + image_name + '2' + extension, dst)
